fd2490391eea4815671ebc1dc02deddb36bbcb06
Franco <phanfranco@yahoo.com>
2021-03-10 13:56:20 -0800

    Finished all routes.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,76 @@
 const router = require("express").Router();
 const workoutDb = require("../models/workout");
 
+router.get("/workouts", (req, res) => {
+  workoutDb
+    .aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ])
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/workouts", (req, res) => {
+  workoutDb
+    .create({})
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+  workoutDb
+    .aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ])
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/workouts/:id", (req, res) => {
+  workoutDb
+    .findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { exercises: req.body },
+      },
+      {
+        new: true,
+      }
+    )
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
